cad16e34c97d0d41e79fc22448bef79c98595722
Lucas Cotrim <lucascotrim3@hotmail.com>
2020-10-02 21:58:54 -0300

    feat: ensure SignUpController returns 400 if an invalid email is provided

--- src/presentation/errors/missing-param-error.ts
+++ src/presentation/errors/missing-param-error.ts
@@ -1,6 +1,6 @@
 export class MissginParamError extends Error {
   constructor (paramName: string) {
     super(`Missing param: ${paramName}`)
-    this.name = ''
+    this.name = 'MissingParamError'
   }
 }
