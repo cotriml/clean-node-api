a0c524882acc46468798005d687e38ebb70a76f2
Lucas Cotrim <lucascotrim3@hotmail.com>
2020-10-05 23:15:34 -0300

    refactor: clear dataBase between tests

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -11,6 +11,11 @@ describe('Account Mongo Repository', () => {
     await MongoHelper.disconnect()
   })
 
+  beforeEach(async () => {
+    const accountColletction = MongoHelper.getCollection('accounts')
+    await accountColletction.deleteMany({})
+  })
+
   const makeSut = (): AccountMongoRepository => {
     return new AccountMongoRepository()
   }
