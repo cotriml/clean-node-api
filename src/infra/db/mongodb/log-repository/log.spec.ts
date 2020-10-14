a01a83fccfb35060961483d36eab8cd63825d387
Lucas Cotrim <lucascotrim3@hotmail.com>
2020-10-13 22:26:50 -0300

    test: ensure MongoRepository creates an error log on success

---
+++ src/infra/db/mongodb/log-repository/log.spec.ts
@@ -0,0 +1,25 @@
+import { Collection } from 'mongodb'
+import { MongoHelper } from '../helpers/mongo-helper'
+import { LogMongoRepository } from './log'
+
+describe('Log Mongo Repository', () => {
+  let errorColletction: Collection
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    errorColletction = await MongoHelper.getCollection('errors')
+    await errorColletction.deleteMany({})
+  })
+  test('Should create an error log on success', async () => {
+    const sut = new LogMongoRepository()
+    await sut.logError('any_error')
+    const count = await errorColletction.countDocuments()
+    expect(count).toBe(1)
+  })
+})
