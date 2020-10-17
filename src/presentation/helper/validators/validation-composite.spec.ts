31891afe0588eebdf2f16e3f5b6889c55d561644
Lucas Cotrim <lucascotrim3@hotmail.com>
2020-10-16 22:02:51 -0300

    refactor: move sut creation to a factory method

--- src/presentation/helper/validators/validation-composite.spec.ts
+++ src/presentation/helper/validators/validation-composite.spec.ts
@@ -2,15 +2,33 @@ import { MissginParamError } from '../../errors'
 import { Validation } from './validation'
 import { ValidationComposite } from './validation-composite'
 
+const makeValidationStub = (): Validation => {
+  class ValidationStub implements Validation {
+    validate (input: any): Error {
+      return null
+    }
+  }
+  return new ValidationStub()
+}
+
+interface SutTypes {
+  sut: ValidationComposite
+  validationStub: Validation
+}
+
+const makeSut = (): SutTypes => {
+  const validationStub = makeValidationStub()
+  const sut = new ValidationComposite([validationStub])
+  return {
+    sut,
+    validationStub
+  }
+}
+
 describe('Validation Composite', () => {
   test('Should return an error if any validation fails', () => {
-    class ValidationStub implements Validation {
-      validate (input: any): Error {
-        return new MissginParamError('field')
-      }
-    }
-    const validationStub = new ValidationStub()
-    const sut = new ValidationComposite([validationStub])
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissginParamError('field'))
     const error = sut.validate({ field: 'any_value' })
     expect(error).toEqual(new MissginParamError('field'))
   })
