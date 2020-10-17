18cb49a8d8cdebf371825a6ebc01bed0e829bb9b
Lucas Cotrim <lucascotrim3@hotmail.com>
2020-10-16 21:18:26 -0300

    refactor: move EmailValidation to the ValidationComposite

--- src/main/factories/signup-validation.spec.ts
+++ src/main/factories/signup-validation.spec.ts
@@ -1,10 +1,21 @@
 import { CompareFieldsValidation } from '../../presentation/helper/validators/compare-fields-validation'
+import { EmailValidation } from '../../presentation/helper/validators/email-validation'
 import { RequiredFieldValidation } from '../../presentation/helper/validators/required-field-validation'
 import { Validation } from '../../presentation/helper/validators/validation'
 import { ValidationComposite } from '../../presentation/helper/validators/validation-composite'
+import { EmailValidator } from '../../presentation/protocols/email-validator'
 import { makeSignupValidation } from './signup-validation'
 
 jest.mock('../../presentation/helper/validators/validation-composite')
+
+const makeEmailValidator = (): EmailValidator => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid (email: string): boolean {
+      return true
+    }
+  }
+  return new EmailValidatorStub()
+}
 
 describe('Signup Validation Factory', () => {
   test('Should call ValidationComposite with all validations', () => {
@@ -14,6 +25,7 @@ describe('Signup Validation Factory', () => {
       validations.push(new RequiredFieldValidation(field))
     }
     validations.push(new CompareFieldsValidation('password', 'passwordConfirmation'))
+    validations.push(new EmailValidation('email', makeEmailValidator()))
     expect(ValidationComposite).toHaveBeenCalledWith(validations)
   })
 })
--- src/main/factories/signup-validation.ts
+++ src/main/factories/signup-validation.ts
@@ -1,8 +1,10 @@
 
 import { CompareFieldsValidation } from '../../presentation/helper/validators/compare-fields-validation'
+import { EmailValidation } from '../../presentation/helper/validators/email-validation'
 import { RequiredFieldValidation } from '../../presentation/helper/validators/required-field-validation'
 import { Validation } from '../../presentation/helper/validators/validation'
 import { ValidationComposite } from '../../presentation/helper/validators/validation-composite'
+import { EmailValidatorAdapter } from '../../utils/email-valitador-adapter'
 
 export const makeSignupValidation = (): ValidationComposite => {
   const validations: Validation[] = []
@@ -10,6 +12,7 @@ export const makeSignupValidation = (): ValidationComposite => {
     validations.push(new RequiredFieldValidation(field))
   }
   validations.push(new CompareFieldsValidation('password', 'passwordConfirmation'))
+  validations.push(new EmailValidation('email', new EmailValidatorAdapter()))
 
   return new ValidationComposite(validations)
 }
