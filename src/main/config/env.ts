5cb33df2a853e44c6a7c5f9f96947ca2789d57d3
Lucas Cotrim <lucascotrim3@hotmail.com>
2020-10-21 22:51:32 -0300

    feat: add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -2,5 +2,6 @@ import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 
 export default {
   mongoUrl: process.env.MONGO_URL || `mongodb+srv://${MongoHelper.remoteDbConfig.username}:${MongoHelper.remoteDbConfig.password}@clean-node-api.0kyhr.gcp.mongodb.net/${MongoHelper.remoteDbConfig.dbName}?retryWrites=true&w=majority`,
-  port: process.env.PORT || 5050
+  port: process.env.PORT || 5050,
+  jwtSecret: process.env.JWT_SECRET || 'ASd=123d'
 }
