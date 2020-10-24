af959b98b59ebb956ebbc3f4d0c709a48efa6229
Lucas Cotrim <lucascotrim3@hotmail.com>
2020-10-24 09:27:20 -0300

    refactor: remove commented line

--- src/presentation/controllers/login/login-controller.spec.ts
+++ src/presentation/controllers/login/login-controller.spec.ts
@@ -59,7 +59,6 @@ describe('Login Controller', () => {
 
   test('Should return 401 if invalid credendials are provided', async () => {
     const { sut, authenticationStub } = makeSut()
-    // jest.spyOn(authenticationStub, 'auth').mockReturnValueOnce(null)
     jest.spyOn(authenticationStub, 'auth').mockReturnValueOnce(new Promise(resolve => resolve(null)))
     const httpResponse = await sut.handle(makeFakeRequest())
     expect(httpResponse).toEqual(unauthorized())
