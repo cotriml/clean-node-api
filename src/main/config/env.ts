265574c8d8a838eb04e71de783e097adbfe064db
Lucas Cotrim <lucascotrim3@hotmail.com>
2020-11-06 08:57:19 -0300

    refactor: change mongodb url

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb+srv://lucas:pIqbs5rrJikKVMe0@clean-node-api.0kyhr.gcp.mongodb.net/clean-node-api?retryWrites=true&w=majority',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://mongo:27017/clean-node-api',
   port: process.env.PORT || 5050,
   jwtSecret: process.env.JWT_SECRET || 'ASd=123d'
 }
