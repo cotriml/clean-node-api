5cd196413f435e0c31ba882a0bf38c1acb5f7544
Lucas Cotrim <lucascotrim3@hotmail.com>
2020-11-28 20:04:49 -0300

    feat: add LogDecorator on AuthMiddleware error

--- src/main/factories/middlewares/auth-middleware-factory.ts
+++ src/main/factories/middlewares/auth-middleware-factory.ts
@@ -1,7 +1,9 @@
 import { makeDbLoadAccountByToken } from '../usecases/account/load-account-by-token/db-load-account-by-token-factory'
 import { AuthMiddleware } from '@/presentation/middlewares/auth-middleware'
 import { Middleware } from '@/presentation/protocols'
+import { makeLogControllerDecorator } from '../decorators/log-controller-decorator-factory'
 
 export const makeAuthMiddleware = (role?: string): Middleware => {
-  return new AuthMiddleware(makeDbLoadAccountByToken(), role)
+  const controller = new AuthMiddleware(makeDbLoadAccountByToken(), role)
+  return makeLogControllerDecorator(controller)
 }
