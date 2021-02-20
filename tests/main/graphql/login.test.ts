b3c5372b99abbebf4215bd2775f1b7cf05db22cf
Lucas Cotrim <lucascotrim3@hotmail.com>
2021-02-20 16:20:27 -0300

    test: ensure Login query returns UnauthorizedError on invalid credentials

--- tests/main/graphql/login.test.ts
+++ tests/main/graphql/login.test.ts
@@ -49,5 +49,17 @@ describe('Login GraphQL', () => {
       expect(res.data.login.accessToken).toBeTruthy()
       expect(res.data.login.name).toBe('Lucas Cotrim')
     })
+
+    test('Should return UnauthorizedError on invalid credentials', async () => {
+      const { query } = createTestClient({ apolloServer })
+      const res: any = await query(loginQuery, {
+        variables: {
+          email: 'lucascotrim3@hotmail.com',
+          password: '123'
+        }
+      })
+      expect(res.data).toBeFalsy()
+      expect(res.errors[0].message).toBe('Unauthorized')
+    })
   })
 })
