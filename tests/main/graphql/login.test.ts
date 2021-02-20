4b2cf722eeea243496449f1aaef977079b881fa6
Lucas Cotrim <lucascotrim3@hotmail.com>
2021-02-20 16:26:26 -0300

    test: ensure SignUp Mutation returns an Account on valid data

--- tests/main/graphql/login.test.ts
+++ tests/main/graphql/login.test.ts
@@ -62,4 +62,28 @@ describe('Login GraphQL', () => {
       expect(res.errors[0].message).toBe('Unauthorized')
     })
   })
+
+  describe('Signup Mutation', () => {
+    const signUpMutation = gql`
+      mutation signUp ($name: String!, $email: String!, $password: String!, $passwordConfirmation: String!){
+        signUp (name: $name, email: $email, password: $password, passwordConfirmation: $passwordConfirmation){
+          accessToken
+          name
+        }
+      }
+    `
+    test('Should return an account on valid data', async () => {
+      const { mutate } = createTestClient({ apolloServer })
+      const res: any = await mutate(signUpMutation, {
+        variables: {
+          name: 'Lucas',
+          email: 'lucascotrim3@hotmail.com',
+          password: '123',
+          passwordConfirmation: '123'
+        }
+      })
+      expect(res.data.signUp.accessToken).toBeTruthy()
+      expect(res.data.signUp.name).toBe('Lucas')
+    })
+  })
 })
