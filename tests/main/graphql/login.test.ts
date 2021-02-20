66bbc834461fa11ff6e9dd38df5d47b98a0f1357
Lucas Cotrim <lucascotrim3@hotmail.com>
2021-02-20 16:30:09 -0300

    test: ensure SignUp Mutation returns EmailInUseError on invalid data

--- tests/main/graphql/login.test.ts
+++ tests/main/graphql/login.test.ts
@@ -85,5 +85,25 @@ describe('Login GraphQL', () => {
       expect(res.data.signUp.accessToken).toBeTruthy()
       expect(res.data.signUp.name).toBe('Lucas')
     })
+
+    test('Should return an EmailInUseError on invalid data', async () => {
+      const password = await hash('123', 12)
+      await accountCollection.insertOne({
+        name: 'Lucas Cotrim',
+        email: 'lucascotrim3@hotmail.com',
+        password: password
+      })
+      const { mutate } = createTestClient({ apolloServer })
+      const res: any = await mutate(signUpMutation, {
+        variables: {
+          name: 'Lucas',
+          email: 'lucascotrim3@hotmail.com',
+          password: '123',
+          passwordConfirmation: '123'
+        }
+      })
+      expect(res.data).toBeFalsy()
+      expect(res.errors[0].message).toBe('The received email is already in use')
+    })
   })
 })
